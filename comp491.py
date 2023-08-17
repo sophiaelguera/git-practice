2bd4c5bf3920e7f9ebf88201fa2ec0186d25f915
MacCormick <jmac@dickinson.edu>
2023-08-17 17:55:37 -0400

    adding initial version of comp491.py, which prints class members

---
+++ comp491.py
@@ -0,0 +1,16 @@
+# Once everyone has contributed their name, this file can be used to print out a list
+# of members of COMP491
+
+def print_names():
+    names = ['John MacCormick',
+             'fake student 1',
+             'fake instructor 1',
+             'fake student 2']
+    print('Here are COMP491 members in alphabetical order:')
+    sorted_names = sorted(names)
+    for name in sorted_names:
+        print(name)
+    print('Good luck to everyone for a great semester!')
+
+
+print_names()
